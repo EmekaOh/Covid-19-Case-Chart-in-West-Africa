fba8cff5a860a32e99a1b7ba6c95f9749d5b4ee5
unknown <ohaerifavour68@gmail.com>
2022-03-28 00:03:34 +0100

    Done with the chart

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let westAfricaCountries = [
 ];
 let covid19Data = [];
 let links = [];
+let confirmedCasesString = [];
 let confirmedCases = [];
 
 let options = {
@@ -54,8 +55,68 @@ for (let i = 0; i < westAfricaCountries.length; i++) {
 
 setTimeout(function () {
   for (let i = 0; i < covid19Data.length; i++) {
-    confirmedCases.push(covid19Data[i].replace(/[^0-9]/g, ""));
+    confirmedCasesString.push(covid19Data[i].replace(/[^0-9]/g, ""));
   }
-  console.log(confirmedCases);
+  //   console.log(confirmedCasesString);
+  confirmedCases = confirmedCasesString.map(Number);
+  Covid19Chart();
+  //   console.log(confirmedCases);
 }, 3000);
 
+function Covid19Chart() {
+  // Chart.defaults.global.defaultFontFamily: 'lato';
+  const labels = westAfricaCountries;
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "West Africa Countries, Covid-19 Confirmed Cases",
+        backgroundColor: [
+          "rgb(255,255,0)",
+          "rgb(255,69,0)",
+          "rgb(189,183,107)",
+          "rgb(0,0,205)",
+          "rgb(138,43,226)",
+          "rgb(0,255,255)",
+          "rgb(255,99,71)",
+          "rgb(255,250,205)",
+          "rgb(230,230,250)",
+          "rgb(139,69,19)",
+          "rgb(124,252,0)",
+          "rgb(139,0,139)",
+          "rgb(216,191,216)",
+          "rgb(0,0,128)",
+        ],
+        borderColor: "rgb(255, 255, 255)",
+        borderWidth: 2,
+        hoverBorderColor: "rgb(0, 0, 0)",
+        data: confirmedCases,
+      },
+    ],
+  };
+
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      plugins: {
+        legend: {
+          labels: {
+            // This more specific font property overrides the global property
+            font: {
+              size: 32,
+              family: 'lato'
+            },
+          },
+        },
+      },
+      layout: {
+        padding: 40,
+      },
+      responsive: true,
+    },
+  };
+
+  const myChart = new Chart(document.getElementById("myChart"), config);
+}
